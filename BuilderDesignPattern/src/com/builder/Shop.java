115f004d31b127660d61b0129dc8ecacc880eafe
Vinod <Vinod@Nagulkar>
2020-05-09 13:24:14 +0530

    Initial commit

---
+++ BuilderDesignPattern/src/com/builder/Shop.java
@@ -0,0 +1,10 @@
+package com.builder;
+
+public class Shop 
+{
+	public static void main(String[] args) 
+	{
+		Phone p=new PhoneBuilder().setOs("Android").setRam(4).getPhone();
+		System.out.println(p);
+	}
+}
